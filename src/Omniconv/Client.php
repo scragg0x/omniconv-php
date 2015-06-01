870980cde3aa49d9cbe6a1b20e17fcabf55cde44
Matt Scragg <matt.scragg@dsisolutions.biz>
2015-06-01 15:36:27 -0500

    add isUp method

--- src/Omniconv/Client.php
+++ src/Omniconv/Client.php
@@ -7,11 +7,21 @@ class Client
     public $client;
     public $baseUri;
 
+    /**
+     * @param string $baseUri
+     */
     public function __construct($baseUri = 'http://omniconv.com')
     {
+        $this->baseUri = $baseUri;
         $this->client = new \GuzzleHttp\Client(['base_url' => $baseUri]);
     }
 
+    /**
+     * @param $toFormat
+     * @param $inFile
+     * @param null $outFile
+     * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
+     */
     public function conv($toFormat, $inFile, $outFile=null)
     {
         $response = $this->client->post('/conv', array(
@@ -26,5 +36,19 @@ class Client
         }
 
         return $response;
+    }
+
+    /**
+     * @param float $timeout seconds
+     * @return bool
+     */
+    public function isUp($timeout=1.0)
+    {
+        try {
+            $response = $this->client->get('/ping', ['timeout' => $timeout]);
+            return ($response->getStatusCode() == 200);
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
