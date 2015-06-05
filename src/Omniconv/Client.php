268339b934f048752b91e91765d76075dff4a87c
Matt Scragg <matt.scragg@dsisolutions.biz>
2015-06-05 12:59:34 -0500

    fix timeout in options add ability to return request object

--- src/Omniconv/Client.php
+++ src/Omniconv/Client.php
@@ -11,7 +11,7 @@ class Client
      * @param string $key
      * @param array $options
      */
-    public function __construct($key=null, $options = array())
+    public function __construct($key = null, $options = array())
     {
         $this->key = $key;
 
@@ -28,19 +28,29 @@ class Client
      * @param $toFormat
      * @param $inFile
      * @param string $outFile
-     * @param int $timeout
+     * @param array $requestOptions
+     * @param bool $returnRequest
      * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
      */
-    public function conv($toFormat, $inFile, $outFile=null, $timeout=0)
+    public function conv($toFormat, $inFile, $outFile = null, $requestOptions = array(), $returnRequest = false)
     {
-        $response = $this->client->post('/conv', array(
+        $defaults = array(
             'headers' => array('X-Omniconv-Key', $this->key),
             'body' => array(
                 'format' => $toFormat,
                 'data' => fopen($inFile, 'r'),
-            ),
-            'timeout' => $timeout
-        ));
+            )
+        );
+
+        $requestOptions = array_replace_recursive($defaults, $requestOptions);
+
+        $request = $this->client->createRequest('POST', '/conv', $requestOptions);
+
+        if ($returnRequest) {
+            return $request;
+        }
+
+        $response = $this->client->send($request);
 
         if ($outFile) {
             file_put_contents($outFile, $response->getBody());
@@ -53,10 +63,11 @@ class Client
      * @param int $timeout milliseconds
      * @return bool
      */
-    public function isUp($timeout=1000)
+    public function isUp($timeout = 2000)
     {
         try {
             $response = $this->client->get('/ping', array('timeout' => $timeout));
+
             return ($response->getStatusCode() == 200);
         } catch (\Exception $e) {
             return false;
